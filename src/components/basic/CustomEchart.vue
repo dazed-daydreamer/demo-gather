e73c72b114a6cc3129d7dbfda69e007d358bc928
梁智豪 <liangzh129@meicloud.com>
2023-06-09 14:58:51 +0800

    [code]侧边菜单栏跳转页面

--- src/components/basic/CustomEchart.vue
+++ src/components/basic/CustomEchart.vue
@@ -21,28 +21,37 @@ const prop = defineProps({
     default: 'auto'
   }
 })
-const chartBox = ref<HTMLElement | null>(null)
+const chartRef = ref<HTMLElement | null>(null)
+let chartContent = reactive<any>(null)
 
 function initChart() {
-  if (!chartBox.value) return
+  if (!chartRef.value) return
   const echarts = getGlobalProperties('$echarts')
-  const myChart = echarts && echarts.init(chartBox.value as HTMLElement, 'dark')
-  if (myChart && prop.option) {
+  chartContent = Object.assign(echarts.init(chartRef.value), chartContent)
+  if (chartContent && prop.option) {
     const option = { ...prop.option }
     if (!option.backgroundColor) {
       option.backgroundColor = '#242424'
     }
-    myChart.setOption(option)
+    chartContent.setOption(option)
   }
+}
+
+function resize() {
+  chartContent.resize()
 }
 
 onMounted(() => {
   initChart()
 })
+
+defineExpose({
+  resize
+})
 </script>
 <template>
   <el-col :span="span">
-    <div ref="chartBox" :style="{ width: width, height: height }"></div>
+    <div ref="chartRef" :style="{ width: width, height: height }"></div>
   </el-col>
 </template>
 <style lang="scss" scoped></style>
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,7 +1,44 @@
 <script setup lang="ts">
 import CustomEchart from '@/components/basic/CustomEchart.vue'
+import _ from 'lodash'
 defineOptions({
   name: 'Home'
+})
+
+let throttled = reactive<any>(null)
+const chartRefOne = ref<InstanceType<typeof CustomEchart> | null>(null)
+const chartRefTwo = ref<InstanceType<typeof CustomEchart> | null>(null)
+const chartRefThree = ref<InstanceType<typeof CustomEchart> | null>(null)
+const chartRefFour = ref<InstanceType<typeof CustomEchart> | null>(null)
+const chartRefFive = ref<InstanceType<typeof CustomEchart> | null>(null)
+const chartRefSix = ref<InstanceType<typeof CustomEchart> | null>(null)
+const chartRefSeven = ref<InstanceType<typeof CustomEchart> | null>(null)
+const chartRefEight = ref<InstanceType<typeof CustomEchart> | null>(null)
+const chartRefNine = ref<InstanceType<typeof CustomEchart> | null>(null)
+
+onMounted(() => {
+  throttled = _.throttle(() => {
+    const chartRefArs = [
+      chartRefOne,
+      chartRefTwo,
+      chartRefThree,
+      chartRefFour,
+      chartRefFive,
+      chartRefSix,
+      chartRefSeven,
+      chartRefEight,
+      chartRefNine
+    ]
+    for (const chartRef of chartRefArs) {
+      chartRef.value && chartRef.value.resize()
+    }
+  }, 600)
+  window.onresize = throttled
+})
+
+onUnmounted(() => {
+  window.onresize = null
+  throttled.cancel()
 })
 
 const topOneOption = {
@@ -25,19 +62,19 @@ const topOneOption = {
 <template>
   <div>
     <el-row>
-      <CustomEchart :option="topOneOption" height="300px" :span="8"></CustomEchart>
-      <CustomEchart :option="topOneOption" height="300px" :span="8"></CustomEchart>
-      <CustomEchart :option="topOneOption" height="300px" :span="8"></CustomEchart>
+      <CustomEchart ref="chartRefOne" :option="topOneOption" height="300px" :span="8"></CustomEchart>
+      <CustomEchart ref="chartRefTwo" :option="topOneOption" height="300px" :span="8"></CustomEchart>
+      <CustomEchart ref="chartRefThree" :option="topOneOption" height="300px" :span="8"></CustomEchart>
     </el-row>
     <el-row>
-      <CustomEchart :option="topOneOption" height="300px" :span="8"></CustomEchart>
-      <CustomEchart :option="topOneOption" height="300px" :span="8"></CustomEchart>
-      <CustomEchart :option="topOneOption" height="300px" :span="8"></CustomEchart>
+      <CustomEchart ref="chartRefFour" :option="topOneOption" height="300px" :span="8"></CustomEchart>
+      <CustomEchart ref="chartRefFive" :option="topOneOption" height="300px" :span="8"></CustomEchart>
+      <CustomEchart ref="chartRefSix" :option="topOneOption" height="300px" :span="8"></CustomEchart>
     </el-row>
     <el-row>
-      <CustomEchart :option="topOneOption" height="300px" :span="8"></CustomEchart>
-      <CustomEchart :option="topOneOption" height="300px" :span="8"></CustomEchart>
-      <CustomEchart :option="topOneOption" height="300px" :span="8"></CustomEchart>
+      <CustomEchart ref="chartRefSeven" :option="topOneOption" height="300px" :span="8"></CustomEchart>
+      <CustomEchart ref="chartRefEight" :option="topOneOption" height="300px" :span="8"></CustomEchart>
+      <CustomEchart ref="chartRefNine" :option="topOneOption" height="300px" :span="8"></CustomEchart>
     </el-row>
   </div>
 </template>
